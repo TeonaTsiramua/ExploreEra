00bf019e58d5a2ecbc7af82f744df0cdc96b3626
TeonaTsiramua <tsiramuateona@gmail.com>
2024-04-11 17:19:41 +0400

    add loader

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,29 @@
+import { useLocation } from 'react-router-dom';
+
 import { Outlet } from 'react-router-dom';
 import Header from './shared/header/Header';
 import Footer from './shared/footer/Footer';
+import { useEffect, useState } from 'react';
+import Loader from './shared/loader/Loader';
 
 const Layout = () => {
-  return (
+  const [loading, setLoading] = useState(true);
+  const location = useLocation();
+
+  useEffect(() => {
+    // Start loading when location changes
+    setLoading(true);
+
+    // Simulate loading delay
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 1500); // 2 seconds for example
+
+    return () => clearTimeout(timeout);
+  }, [location]);
+  return loading ? (
+    <Loader />
+  ) : (
     <>
       <Header />
       <Outlet />
