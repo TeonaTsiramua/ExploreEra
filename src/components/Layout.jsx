5976e06325b0eba86b0919b227822b5c1b8adb87
TeonaTsiramua <tsiramuateona@gmail.com>
2024-04-11 19:18:31 +0400

    add loader

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,25 @@
 import { Outlet } from 'react-router-dom';
 import Header from './shared/header/Header';
 import Footer from './shared/footer/Footer';
+import { useEffect, useState } from 'react';
+import Loader from './shared/loader/Loader';
 
 const Layout = () => {
-  return (
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+
+    // Simulate loading delay
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+
+    return () => clearTimeout(timeout);
+  }, []);
+  return loading ? (
+    <Loader />
+  ) : (
     <>
       <Header />
       <Outlet />
